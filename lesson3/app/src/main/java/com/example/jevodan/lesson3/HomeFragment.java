22d5f26aad98308cd1e3abe2f9a69d939a373936
Jevodan <sanne80@mail.ru>
2019-05-26 13:10:36 +0300

    lesson3

---
+++ lesson3/app/src/main/java/com/example/jevodan/lesson3/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.jevodan.lesson3;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    public ImageView imageVieww;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        imageVieww = view.findViewById(R.id.imageView);
+        return view;
+    }
+
+}
